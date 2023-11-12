2874bc673d2caad92763b3d148c9082146a936f9
Pedro Antunes <pedronmantunes@gmail.com>
2023-11-12 04:38:23 +0100

    IceSugar_pro.mk: - grab lpf file under current directory; - use fpga sources.

--- Board/IceSugar_pro.mk
+++ Board/IceSugar_pro.mk
@@ -1,9 +1,12 @@
 FPGA ?= ecp5
 PROJECT_FPGA_TOP ?= $(PROJECT_NAME)_$(FPGA)
 PROJECT_FPGA_DIR ?= $(PROJECT_RTL_DIR)/$(FPGA)
-PROJECT_LPF ?= $(PROJECT_FPGA_DIR)/$(PROJECT_NAME)_$(BOARD).lpf
 
-FPGA_SOURCES := $(wildcard $(PROJECT_FPGA_DIR)/*)
+# Use find to search for the file recursively
+PROJECT_LPF ?= $(wildcard $(shell find $(CURDIR) -type f -name $(PROJECT_NAME)_$(BOARD).lpf))
+PROJECT_LPF := $(firstword $(PROJECT_LPF))
+
+FPGA_SOURCES := $(wildcard $(PROJECT_FPGA_DIR)/*) $(VERILOG_SOURCES)
 
 OUTPUT_FPGA_DIR ?= $(PROJECT_DIR)/$(FPGA)
 PROJECT_JSON := $(OUTPUT_FPGA_DIR)/$(PROJECT_NAME).json
@@ -24,7 +27,7 @@ $(OUTPUT_FPGA_DIR):
 
 # fpga-synthesis
 $(PROJECT_JSON): $(OUTPUT_FPGA_DIR)
-	yosys -p "synth_ecp5 -top $(PROJECT_FPGA_TOP) -json $@" $(VERILOG_SOURCES)
+	yosys -p "synth_ecp5 -top $(PROJECT_FPGA_TOP) -json $@" $(FPGA_SOURCES)
 
 # fpga-place_and_route
 $(PROJECT_PNR): $(PROJECT_JSON)
