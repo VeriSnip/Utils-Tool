8b8de0ba17a0ded8a0a9576152c146f53dfdb876
Pedro Antunes <pedronmantunes@gmail.com>
2025-02-02 15:13:29 +0100

    (fix: use help target as default Makefile target)

--- utils.mk
+++ utils.mk
@@ -8,12 +8,12 @@ PROJECT_RTL_DIR ?= $(PROJECT_BUILD_DIR)/RTL
 
 VERILOG_SOURCES := $(wildcard $(PROJECT_RTL_DIR)/*.v $(PROJECT_RTL_DIR)/*.sv)
 
-nix:
-	nix-shell $(MyUtils_DIR)/shell.nix
-
 help:
 	@echo "Please use \`make <target>' where <target> is one of"
 	@echo "  build          to create a directory with the built rtl code."
+
+nix:
+	nix-shell $(MyUtils_DIR)/shell.nix
 
 clean-all: sim-clean board-clean hardware-clean 
 
